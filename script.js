1c3bbe86ff57c89561625127ef73195f42c3b343
Daolaiya <daolaiya@gmail.com>
2025-12-15 17:52:39 -0500

    Commit made at 5:52PM on 2025-12-15

--- script.js
+++
@@ -1,9 +0,0 @@
-const button = document.getElementById("clickMeBtn");
-const output = document.getElementById("output");
-
-let count = 0;
-
-button.addEventListener("click", () => {
-  count += 1;
-  output.textContent = `Button clicked ${count} time${count === 1 ? "" : "s"}.`;
-});
